d8e572a483a9e675c9cad945b6e70bc75c84190f
CodersArts <35237091+CodersArts@users.noreply.github.com>
2020-06-05 18:41:57 +0530

    Create FurnitureApp.java

---
+++ JavaFx/FurnitureApp/FurnitureApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package furnitureapp;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author 44750
+ */
+public class FurnitureApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI main = new GUI();
+    }
+
+}
